2f6cbd5fd8a69b2c3e105da7834740821d675eb6
tomasmorlalopez <tomasmorlalopez>
2024-04-08 18:59:04 +0000

    add py test file

---
+++ notebookTest.py
@@ -0,0 +1,35 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC # programa hello world
+
+# COMMAND ----------
+
+# DBTITLE 1,mi primer programa
+# Define a function named hello_world
+def hello_world():
+    # Print "Hello, World!"
+    print("Hello, World!")
+
+# Call the hello_world function
+hello_world()
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC # Header 1
+# MAGIC ## Header 2
+# MAGIC **bold text**
+# MAGIC *italics text*
+# MAGIC ~~strikethrough text~~
+# MAGIC `monospace text`
+# MAGIC ---
+# MAGIC > Block quote
+# MAGIC Ordered list:
+# MAGIC 1. Item 1
+# MAGIC 1. Item 2
+# MAGIC 1. Item 3
+# MAGIC Unordered list:
+# MAGIC - Item a
+# MAGIC - Item b
+# MAGIC - Item c
+# MAGIC
